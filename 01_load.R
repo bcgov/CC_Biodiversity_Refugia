050b361ce0651cf6470ef510db8a6aec57051925
Firstname Lastname <firstname.lastname@gov.bc.ca>
2020-03-18 18:43:19 -0700

    code updates

--- 01_load.R
+++ 01_load.R
@@ -1,12 +1,150 @@
 # Copyright 2020 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+
+source("header.R")
+
+# bring in BC boundary
+bc <- bcmaps::bc_bound()
+Prov_crs<-crs(bc)
+#Prov_crs<-"+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+
+#Provincial BEC 1970 and 2080
+BEC1970P<-raster(file.path(RefugiaSpatialDir, 'BEC_zone_1970/BEC_zone.tif')) %>%
+  projectRaster(crs=Prov_crs, method='ngb')
+saveRDS(BEC1970P,file='tmp/BEC1970P')
+#Check raster in Q compare with vector BEC
+writeRaster(BEC1970P, filename=file.path(spatialOutDir,paste("BEC1970P",sep="")), format="GTiff",overwrite=TRUE)
+
+BEC1970P_LUT <- read.dbf(file.path(RefugiaSpatialDir, "BEC_zone_1970/BEC_zone.tif.vat.dbf"),as.is=TRUE)
+
+BEC2080P<-raster(file.path(RefugiaSpatialDir, 'BEC_zone_2080s/BEC_zone_2080s.tif')) %>%
+  projectRaster(crs=Prov_crs, method='ngb')
+saveRDS(BEC2080P,file='tmp/BEC2080P')
+
+BEC2080P_LUT <- read.dbf(file.path(RefugiaSpatialDir, "BEC_zone_2080s/BEC_zone_2080s.tif.vat.dbf"),as.is=TRUE)
+
+#Make new universal LUT since values different in 2 rasters
+BEC_LUT<-BEC1970P_LUT %>%
+  full_join(BEC2080P_LUT,by='VAR') %>%
+  mutate(ID = 1:n()) %>%
+  mutate(VARn = ID) %>%
+  transform(SUBZn=as.integer(factor(SUBZ))) %>%
+  transform(ZONEn=as.integer(factor(ZONE.x))) %>%
+  dplyr::select(ID, V1970 = VALUE.x, V2080 = VALUE.y, ZONEn, ZONE=ZONE.x, SUBZn, SUBZ, VARn, VAR)
+
+#Reclass rasters so values are the same for Variants
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$V1970)),] %>%
+  dplyr::select(V1970,ID)
+BEC1970PS <- raster::subs(BEC1970P, subsBEC)
+saveRDS(BEC1970PS,file='tmp/BEC1970PS')
+writeRaster(BEC1970PS, filename=file.path(spatialOutDir,paste("BEC1970PS",sep="")), format="GTiff",overwrite=TRUE, RAT=TRUE)
+
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$V2080)),] %>%
+  dplyr::select(V2080,ID)
+BEC2080PS <- raster::subs(BEC2080P, subsBEC)
+saveRDS(BEC2080PS,file='tmp/BEC2080PS')
+writeRaster(BEC2080PS, filename=file.path(spatialOutDir,paste("BEC2080PS",sep="")), format="GTiff",overwrite=TRUE, RAT=TRUE)
+
+#Read in BEC grouping spredsheet -BECv11_SubzoneVariant_Groups.xlsx
+BECgroupSheets<- excel_sheets(file.path(DataDir,'BECv11_SubzoneVariant_Groups.xlsx'))
+BECgroupSheetsIn<-read_excel(file.path(DataDir,'BECv11_SubzoneVariant_Groups.xlsx'),
+                             sheet = BECgroupSheets[2])
+#Make a data frame for the data
+BECGroup_LUT<-data.frame(VARns=BECgroupSheetsIn$`BEC Unit`,
+                         BECgroup=BECgroupSheetsIn$GROUP, stringsAsFactors = FALSE)
+#Trim whitespace in BEC_LUT bec labels so can match to Group_LUT
+BEC_LUT$VARns<-gsub(" ", "", BEC_LUT$VAR, fixed = TRUE)
+#Join to BEC_LUT
+BEC_LUT<-BEC_LUT %>%
+  left_join(BECGroup_LUT)
+#Assign NAs groups to ZONEs
+  BEC_LUT$BECgroup[is.na(BEC_LUT$BECgroup)] <-
+    paste(as.character(BEC_LUT$ZONE[is.na(BEC_LUT$BECgroup)]),'_missing',sep='')
+BEC_LUT<-BEC_LUT %>%
+  transform(Groupn=as.integer(factor(BECgroup)))
+
+
+# Download BEC - # Gets bec_sf zone shape and filters the desired subzones
+bec_sf <- bec(class = "sf")# %>%
+  #st_intersection(study_area)
+saveRDS(bec_sf, file = "tmp/bec_sf")
+
+ws <- get_layer("wsc_drainages", class = "sf") %>%
+  select(SUB_DRAINAGE_AREA_NAME, SUB_SUB_DRAINAGE_AREA_NAME) %>%
+  filter(SUB_DRAINAGE_AREA_NAME %in% c("Nechako", "Skeena - Coast"))
+st_crs(ws)<-3005
+saveRDS(ws, file = "tmp/ws")
+
+#Read in Landform file and mask to ESI area
+LForm<-
+  #raster(file.path('../GB_Data/data/Landform',"Landform_BCAlbs.tif")) %>%
+  raster(file.path('/Users/darkbabine/Dropbox (BVRC)/_dev/Bears/GB_Data/data/Landform',"LForm.tif"))
+#mapview(LForm, maxpixels =  271048704)
+
+#LFormFlat[!(LFormFlat[] %in% c(1000,5000,6000,7000,8000))]<-NA
+
+#      ID	Landform	colour
+#   1000	 Valley	 #358017
+#   2000	 Hilltop in Valley	 #f07f21
+#   3000	 Headwaters	 #7dadc3
+#   4000	 Ridges and Peaks	 #ebebf1
+#   5000	 Plains	 #c9de8d
+#   6000	 Local Ridge in Plain	 #f0b88a
+#   7000	 Local Valley in Plain	 #4cad25
+#   8000	 Gentle Slopes	 #bbbbc0
+#   9000	 Steep Slopes	 #8d8d91
+
+LForm_LUT <- data.frame(LFcode = c(1000,2000,3000,4000,5000,6000,7000,8000,9000),
+                        Landform = c('Valley','Hilltop in Valley','Headwaters','Ridges and Peaks',
+                                     'Plains','Local Ridge in Plain','Local Valley in Plain',
+                                     'Gentle Slopes','Steep Slopes'),
+                        colourC = c('#358017','#f07f21','#7dadc3','#ebebf1','#c9de8d','#f0b88a',
+                                    '#4cad25','#bbbbc0','#8d8d91'))
+saveRDS(LForm_LUT, file = 'tmp/LForm_LUT')
+
+LandCover<-
+  raster(file.path(ESIDir,'Data/DataScience/SkeenaESI_LandCover_Age_Human_Footprint/OutRaster','LandCover.tif'))
+LandCover_LUT <- read_excel(file.path(ESIDir,'Data/DataScience/SkeenaESI_LandCover_Age_Human_Footprint/LUT','LandCoverLookUp_LUT.xlsx'),sheet=1)
+saveRDS(LandCover, file = 'tmp/LandCover_LUT')
+
+Age<-
+  raster(file.path(ESIDir,'Data/DataScience/SkeenaESI_LandCover_Age_Human_Footprint/OutRaster','Age.tif'))
+saveRDS(Age, file = 'tmp/Age')
+
+ExtensiveFootprint<-
+  raster(file.path(ESIDir,'Data/DataScience/SkeenaESI_LandCover_Age_Human_Footprint/OutRaster','ExtensiveFootprint.tif'))
+saveRDS(ExtensiveFootprint, file = 'tmp/ExtensiveFootprint')
+
+ExtensiveFootprint_LUT <- read_csv(file.path(ESIDir,'Data/DataScience/SkeenaESI_LandCover_Age_Human_Footprint/LUT','ExtensiveFootprintStatsTableRows.csv'))
+saveRDS(ExtensiveFootprint_LUT, file = 'tmp/ExtensiveFootprint_LUT')
+
+#Other load stuff
+if(!is.null(subzones)){
+  bec_sf <- dplyr::filter(bec_sf, as.character(MAP_LABEL) %in% subzones)
+  study_area <- st_intersection(study_area, bec_sf) %>% # The AOI is trimmed according to the bec_sf zones included
+    summarise()
+  st_write(study_area, file.path(res_folder, "AOI.gpkg"), delete_layer = TRUE)
+
+#ESI boundary - for testing, etc.
+ESI_file <- file.path("tmp/ESI")
+if (!file.exists(ESI_file)) {
+  #Load ESI boundary
+  ESIin <- read_sf(file.path(ESIDir,'Data/Skeena_ESI_Boundary'), layer = "ESI_Skeena_Study_Area_Nov2017") %>%
+    st_transform(3005)
+  ESI <- st_cast(ESIin, "MULTIPOLYGON")
+  saveRDS(ESI, file = ESI_file)
+} else
+  ESI<-readRDS(file = ESI_file)
+
+study_area <- ESI
+
 
--- 03_analysis.R
+++ 03_analysis.R
@@ -1,12 +1,231 @@
 # Copyright 2020 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+
+source('header.R')
+
+#Compare BEC from 1970 to 2080 - Zone, Sub-Zone and Variant
+
+### Zone
+#Compare Zone from 1970 to 2080
+#Change rasters to be of Zone instead of default Variant
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,ZONEn)
+BEC1970z<- raster::subs(BEC1970, subsBEC)
+writeRaster(BEC1970z, filename=file.path(spatialOutDir,paste("BEC1970z",sep="")), format="GTiff",overwrite=TRUE)
+
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,ZONEn)
+BEC2080z<- raster::subs(BEC2080, subsBEC)
+writeRaster(BEC2080z, filename=file.path(spatialOutDir,paste("BEC2080z",sep="")), format="GTiff",overwrite=TRUE)
+
+#Crosstab to see extent of overlap
+#create LUT for each year
+BEC1970z_LUT<-BEC1970z %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(ZONEn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(ZONEn.1=ZONEn) %>%
+  dplyr::summarise(ZONE=first(ZONE)) %>%
+  rbind(data.frame(ZONEn.1=NA, ZONE=NA))
+
+BEC2080z_LUT<-BEC2080z %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(ZONEn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(ZONEn.2=ZONEn) %>%
+  dplyr::summarise(ZONE=first(ZONE)) %>%
+  rbind(data.frame(ZONEn.2=NA, ZONE=NA))
+
+#need to add NA case since many non-analogue between years
+BEC1970_2080z<-raster::crosstab(BEC1970z, BEC2080z, long=TRUE, useNA=TRUE) %>%
+  left_join(BEC1970z_LUT) %>%
+  dplyr::rename(BEC1970=ZONE) %>%
+  left_join(BEC2080z_LUT) %>%
+  dplyr::rename(BEC2080=ZONE) %>%
+  dplyr::select(BEC1970n=ZONEn.1, BEC1970, BEC2080n=ZONEn.2, BEC2080, Freq)
+
+sum(BEC1970_2080z$Freq)
+
+#Find which and the number of overlaps between years
+macroRefugiaZ <- BEC1970_2080z %>%
+  dplyr::filter_(~as.character(BEC1970) == as.character(BEC2080))
+#Calculate % of overlap between 1970 and 2080
+sum(macroRefugiaZ$Freq)/sum(BEC1970_2080z$Freq)*100
+
+### Sub-Zone
+#Compare Sub=Zone from 1970 to 2080
+#Change rasters to be of Sub-Zone
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,SUBZn)
+BEC1970sz<- raster::subs(BEC1970, subsBEC)
+writeRaster(BEC1970sz, filename=file.path(spatialOutDir,paste("BEC1970sz",sep="")), format="GTiff",overwrite=TRUE)
+
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,SUBZn)
+BEC2080sz<- raster::subs(BEC2080, subsBEC)
+writeRaster(BEC2080sz, filename=file.path(spatialOutDir,paste("BEC2080sz",sep="")), format="GTiff",overwrite=TRUE)
+
+#Crosstab to see extent of overlap
+#create LUT for each year
+BEC1970sz_LUT<-BEC1970sz %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(SUBZn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(SUBZn.1=SUBZn) %>%
+  dplyr::summarise(SUBZ=first(SUBZ)) %>%
+  rbind(data.frame(SUBZn.1=NA, SUBZ=NA))
+
+BEC2080sz_LUT<-BEC2080sz %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(SUBZn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(SUBZn.2=SUBZn) %>%
+  dplyr::summarise(SUBZ=first(SUBZ)) %>%
+  rbind(data.frame(SUBZn.2=NA, SUBZ=NA))
+
+#need to add NA case since many non-analogue between years
+BEC1970_2080sz<-raster::crosstab(BEC1970sz, BEC2080sz, long=TRUE, useNA=TRUE) %>%
+  left_join(BEC1970sz_LUT) %>%
+  dplyr::rename(BEC1970=SUBZ) %>%
+  left_join(BEC2080sz_LUT) %>%
+  dplyr::rename(BEC2080=SUBZ) %>%
+  dplyr::select(BEC1970n=SUBZn.1, BEC1970, BEC2080n=SUBZn.2, BEC2080, Freq)
+
+sum(BEC1970_2080sz$Freq)
+
+#Find which and the number of overlaps between years
+macroRefugiaSZ <- BEC1970_2080sz %>%
+  dplyr::filter_(~as.character(BEC1970) == as.character(BEC2080))
+#Calculate % of overlap between 1970 and 2080
+sum(macroRefugiaSZ$Freq)/sum(BEC1970_2080sz$Freq)*100
+
+### Variant
+#Compare Variant from 1970 to 2080
+#Change rasters to be of Variant
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,VARn)
+BEC1970v<- raster::subs(BEC1970, subsBEC)
+writeRaster(BEC1970v, filename=file.path(spatialOutDir,paste("BEC1970v",sep="")), format="GTiff",overwrite=TRUE)
+
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,VARn)
+BEC2080v<- raster::subs(BEC2080, subsBEC)
+writeRaster(BEC2080v, filename=file.path(spatialOutDir,paste("BEC2080v",sep="")), format="GTiff",overwrite=TRUE)
+
+#Crosstab to see extent of overlap
+#create LUT for each year
+BEC1970v_LUT<-BEC1970v %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(VARn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(VARn.1=VARn) %>%
+  dplyr::summarise(VAR=first(VAR)) %>%
+  rbind(data.frame(VARn.1=NA, VAR=NA))
+
+BEC2080v_LUT<-BEC2080v %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(VARn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(VARn.2=VARn) %>%
+  dplyr::summarise(VAR=first(VAR)) %>%
+  rbind(data.frame(VARn.2=NA, VAR=NA))
+
+#need to add NA case since many non-analogue between years
+BEC1970_2080v<-raster::crosstab(BEC1970v, BEC2080v, long=TRUE, useNA=TRUE) %>%
+  left_join(BEC1970v_LUT) %>%
+  dplyr::rename(BEC1970=VAR) %>%
+  left_join(BEC2080v_LUT) %>%
+  dplyr::rename(BEC2080=VAR) %>%
+  dplyr::select(BEC1970n=VARn.1, BEC1970, BEC2080n=VARn.2, BEC2080, Freq)
+
+sum(BEC1970_2080v$Freq)
+
+#Find which and the number of overlaps between years
+macroRefugiaV <- BEC1970_2080v %>%
+  dplyr::filter_(~as.character(BEC1970) == as.character(BEC2080))
+#Calculate % of overlap between 1970 and 2080
+sum(macroRefugiaV$Freq)/sum(BEC1970_2080v$Freq)*100
+
+### Group
+#Compare BEC groups from 1970 to 2080
+#Change rasters to be of Groups
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,Groupn)
+BEC1970g<- raster::subs(BEC1970, subsBEC)
+writeRaster(BEC1970g, filename=file.path(spatialOutDir,paste("BEC1970g",sep="")), format="GTiff",overwrite=TRUE)
+
+subsBEC<-BEC_LUT[!(is.na(BEC_LUT$ID)),] %>%
+  dplyr::select(ID,Groupn)
+BEC2080g<- raster::subs(BEC2080, subsBEC)
+writeRaster(BEC2080g, filename=file.path(spatialOutDir,paste("BEC2080g",sep="")), format="GTiff",overwrite=TRUE)
+
+#Crosstab to see extent of overlap
+#create LUT for each year
+BEC1970g_LUT<-BEC1970g %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(Groupn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(Groupn.1=Groupn) %>%
+  dplyr::summarise(BECgroup=first(BECgroup)) %>%
+  rbind(data.frame(Groupn.1=NA, BECgroup=NA))
+
+BEC2080g_LUT<-BEC2080g %>%
+  unique() %>%
+  data.frame() %>%
+  dplyr::rename(Groupn ='.') %>%
+  left_join(BEC_LUT) %>%
+  group_by(Groupn.2=Groupn) %>%
+  dplyr::summarise(BECgroup=first(BECgroup)) %>%
+  rbind(data.frame(Groupn.2=NA, BECgroup=NA))
+
+#need to add NA case since many non-analogue between years
+BEC1970_2080g<-raster::crosstab(BEC1970g, BEC2080g, long=TRUE, useNA=TRUE) %>%
+  left_join(BEC1970g_LUT) %>%
+  dplyr::rename(BEC1970=BECgroup) %>%
+  left_join(BEC2080g_LUT) %>%
+  dplyr::rename(BEC2080=BECgroup) %>%
+  dplyr::select(BEC1970n=Groupn.1, BEC1970, BEC2080n=Groupn.2, BEC2080, Freq)
+
+sum(BEC1970_2080g$Freq)
+
+#Find which and the number of overlaps between years
+macroRefugiaG <- BEC1970_2080g %>%
+  dplyr::filter_(~as.character(BEC1970) == as.character(BEC2080))
+#Calculate % of overlap between 1970 and 2080
+sum(macroRefugiaG$Freq)/sum(BEC1970_2080g$Freq)*100
+
+#Output data as a multi-tabbed spreadsheet
+XtabSummary<- data.frame(c('Zone','SubZone','Variant','Group'),
+                         c(sum(macroRefugiaZ$Freq)/sum(BEC1970_2080z$Freq)*100,
+                           sum(macroRefugiaSZ$Freq)/sum(BEC1970_2080sz$Freq)*100,
+                           sum(macroRefugiaV$Freq)/sum(BEC1970_2080v$Freq)*100,
+                           sum(macroRefugiaG$Freq)/sum(BEC1970_2080g$Freq)*100))
+colnames(XtabSummary)<-c('BECscale','pcOverlap')
+
+
+#mapview(BEC1970z,maxpixels =  4308570)+mapview(BEC2080z,maxpixels =  4308570)
+
+#Write out results into a multi-tab spreadsheet
+BECxData<-list(XtabSummary, BEC1970_2080v,macroRefugiaV, BEC1970_2080sz,macroRefugiaSZ, BEC1970_2080z,macroRefugiaZ)
+BECDataNames<-c('XtabSummary','VariantXtab','VariantOverlap','SubZoneXtab','SubZoneOverlap','ZoneXtab','ZoneOverlap')
+
+WriteXLS(BECxData,file.path(dataOutDir,paste('BECxData.xlsx',sep='')),SheetNames=BECDataNames)
+
+
 
